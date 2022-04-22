3c59738707679e2d9b7d6da3d275d9d3b5deb432
exing1 <edward_xing@brown.edu>
2022-04-21 21:11:07 -0400

    plot psnr

--- eval.py
+++ eval.py
@@ -2,6 +2,7 @@
 this file handles the evaluation of the debluring methods
 """
 from matplotlib import pyplot as plt
+from get_data import parse_dataset
 
 
 def get_average_psnr(img_list):
@@ -16,11 +17,13 @@ def get_average_psnr(img_list):
     pass
 
 
-def main():
+def main(data_path='data/original_images/'):
     """
-    plot the results of average psnr"""
-    # TODO: Ed
-    pass
+    plot the results of average psnr
+    """
+    blurred_images = parse_dataset(data_path)  # there should be 15 of them
+    res = get_average_psnr(blurred_images)
+    
 
 
 if __name__ == "__main__":
