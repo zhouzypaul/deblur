5ad2d19f0edb64efb35ae618b7d8bbd6d33ea4f0
exing1 <edward_xing@brown.edu>
2022-04-25 15:08:11 -0400

    eval plot

--- eval.py
+++ eval.py
@@ -1,29 +1,38 @@
 """
 this file handles the evaluation of the debluring methods
 """
+from cv2 import blur
 from matplotlib import pyplot as plt
 from get_data import parse_dataset
+import numpy as np
 
 
-def get_average_psnr(img_list):
+def get_average_psnr(img):
     """
-    this function calculates the average psnr of a list of images
+    this function calculates the average psnr of an image
     args:
-        img_list: a list of images (should be 15 of them from the paper's dataset)
+        img: a list of images produced by different blurs on the same image
     return:
-        the average psnr of the list of images
+        the average psnr
     """
     # TODO: Alan
-    pass
+    return 10
 
 
 def main(data_path='data/original_images/'):
     """
     plot the results of average psnr
     """
-    blurred_images = parse_dataset(data_path)  # there should be 15 of them
-    res = get_average_psnr(blurred_images)
-    
+    blurred_images = np.zeros(10)  # there should be 15 of them
+    psnr = []
+    labels = []
+
+    for i, img in enumerate(blurred_images):
+        psnr.append(get_average_psnr(img))
+        labels.append(str(i + 1))
+
+    plt.bar(labels, psnr)
+    plt.show()
 
 
 if __name__ == "__main__":
