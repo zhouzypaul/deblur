434ad43c5d36629b46b238936a940966a7d97a34
agu101 <alan_gu@brown.edu>
2022-05-07 15:19:38 -0400

    commented params.py

--- params.py
+++ params.py
@@ -2,8 +2,17 @@
 Hyperparameters
 """
 
+# latent image estimation regularization factor
 lmda = 4e-3
+
+# blur kernel estimation regularization factor
 gamma = 2
+
+# latent image estimation smoothing factor
 sigma = 1
+
+# latent image estimation convergence condition
 beta_max = 2**3
+
+# latent image estimation intermediate convergence condition
 miu_max = 1e5
